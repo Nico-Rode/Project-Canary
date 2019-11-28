1b76f7c2225d5f1ba712ec3d02a01009f51d962b
Nico <nrode@tibco.com>
2019-11-28 13:27:25 -0800

    switched to naive CNN -- started initial frame predictions

--- CNNDefenseDetection.py
+++ CNNDefenseDetection.py
@@ -2,8 +2,15 @@ from numpy import array
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Input, TimeDistributed, Dropout
-from keras.layers import Convolution2D, MaxPooling2D, Flatten, GlobalAveragePooling2D
+from keras.layers import Convolution2D, MaxPooling2D, Flatten, GlobalAveragePooling2D, Activation
 import pickle
+import os
+from datetime import datetime
+
+
+logdir = "logs/scalars/" + "nr112619"
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+
 
 
 
@@ -11,47 +18,77 @@ import pickle
 print("Keras version = {}".format(keras.__version__))
 
 
-layer = Convolution2D(64, (3, 3), activation='relu')
 
-model = Sequential()
+def build_time_distributed_model():
+    layer = Convolution2D(64, (3, 3), activation='relu')
 
-input = TimeDistributed(layer, input_shape = (150, 224, 224, 1))
-# input, with 64 convolutions for 5 images
-# that have (224, 224, 3) shape
-model.add(input)
+    model = Sequential()
+
+    input = TimeDistributed(layer, input_shape = (150, 224, 224, 1))
+    # input, with 64 convolutions for 5 images
+    # that have (224, 224, 3) shape
+    model.add(input)
 
 
-model.add(
-    TimeDistributed( 
-        Convolution2D(64, (3,3), activation='relu')
+    model.add(
+        TimeDistributed( 
+            Convolution2D(64, (3,3), activation='relu')
+        )
     )
-)
-model.add(
-    TimeDistributed(
-        GlobalAveragePooling2D()
+    model.add(
+        TimeDistributed(
+            GlobalAveragePooling2D()
+        )
     )
-)
 
-model.add(
-    LSTM(1024, activation='relu', return_sequences=False)
-)
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(.5))
-# For example, for 3 outputs classes 
-model.add(Dense(3, activation='sigmoid'))
-model.compile('adam', loss='categorical_crossentropy')
-
-print("Here we are...")
+    model.add(
+        LSTM(1024, activation='relu', return_sequences=False)
+    )
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(.5))
+    # For example, for 3 outputs classes 
+    model.add(Dense(3, activation='sigmoid'))
+    model.compile('adam', loss='categorical_crossentropy')
 
 
+    return model
 
-# pickle_in = open("X.pickle","rb")
-# X = pickle.load(pickle_in)
 
-# pickle_in = open("y.pickle","rb")
-# y = pickle.load(pickle_in)
+def build_naive_model(X):
 
-# model.fit(X,y, batch_size=32, epochs=3, validation_split=.3)
+    model = Sequential()
+
+    model.add(Convolution2D(224, (3, 3), input_shape=X.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Convolution2D(224, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+
+    model.add(Dense(64))
+
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                optimizer='adam',
+                metrics=['accuracy'])
+
+    model.save("nflDefenseDetectionModel.h5")
+    return model
+
+
+
+def train_model(model, X, y):
+    model.fit(X, y, batch_size=64, epochs=2, validation_split=0.3, callbacks=[tensorboard_callback])
+    return model
+
+
+def predict_model(model, X):
+   return model.predict_classes(X) 
 
 
 
--- main.py
+++ main.py
@@ -1,26 +1,50 @@
 from readFiles import get_list_of_raw_videos, trim_videos_in_list
 from vectorizeFiles import extract_frames_from_trimmed_videos 
-from extractFeaturesVectorizedVideos import create_training_data
+from extractFeaturesVectorizedVideos import create_training_data, test_testing_data
+from CNNDefenseDetection import build_naive_model, train_model, predict_model
+import os
+import numpy as np
+import pickle
+from keras.models import load_model
+from cv2 import cv2
 
 def main():
-    print("starting program")
-    directoryPath = "C:\\Users\\Nico Rode\\Desktop\\NFLAnalysis"
+    print("starting program")    
+    PCDirectoryPath = "C:\\Users\\Nico Rode\\Desktop\\NFLAnalysis"
+    macDirectoryPath = os.path.join("/","Users", "nicholasrode", "Desktop", "ProjectCanary")
     firstTimeBool = False
 
-    prep_videos(directoryPath, firstTimeBool)
+    prep_videos(macDirectoryPath, firstTimeBool)
 
+    
 
 
 
 def prep_videos(directoryPath, firstTimeBool):
+
+
+
     if firstTimeBool:
         listOfRawVideos = get_list_of_raw_videos("{}\\{}".format(directoryPath, "NFLRawVideos"))
         trim_videos_in_list(listOfRawVideos, directoryPath)
         extract_frames_from_trimmed_videos(directoryPath)
-    create_training_data(directoryPath)
+        create_training_data(directoryPath)
+
+        pickle_in = open("X.pickle","rb")
+        X = pickle.load(pickle_in)
+
+        pickle_in = open("y.pickle","rb")
+        y = pickle.load(pickle_in)
+
+        X = X/255.0
+
+        model = build_naive_model(X)
+
+        model = train_model(model, X, y)
 
 
 
+    test_testing_data(directoryPath)
 
 
 if __name__ =="__main__":
